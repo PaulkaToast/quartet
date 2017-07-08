09090a8c990e5f5304602ee28f1ccd06237e2a4d
Ammar Askar <ammar@ammaraskar.com>
2017-07-07 18:12:16 -0700

    Changed the structure of how circles are drawn

--- frontend.js
+++ frontend.js
@@ -1,14 +1,55 @@
-function draw() {
-    var canvas = document.getElementById('tutorial');
-    var ctx = canvas.getContext('2d');
+//variables//
 
-    for (var i = 0; i < 6; i++) {
-      for (var j = 0; j < 6; j++) {
-        ctx.strokeStyle = 'rgb(0, ' + Math.floor(255 - 42.5 * i) + ', ' + 
-                         Math.floor(255 - 42.5 * j) + ')';
+//Classes//
+class Circle {
+
+    constructor(x, y) {
+        this.radius = 30; //radius 
+        this.x = 40 + x * 80; //x cordinate
+        this.y = 40 + y * 70; //y cordinate
+        this.r = 0; //red
+        this.b = Math.floor(255 - 25 * x); //blue
+        this.g = Math.floor(255 - 25 * y); //green
+        this.a = 0.5; //aplha
+    }
+
+    draw() {
+        ctx.strokeStyle = 'rgba('     + this.r 
+                                + ',' + this.b 
+                                + ',' + this.g 
+                                + ',' + this.a 
+                                + ')';
         ctx.beginPath();
-        ctx.arc(12.5 + j * 25, 12.5 + i * 25, 10, 0, Math.PI * 2, true);
-        ctx.stroke();
+        ctx.arc(this.y, this.x, this.radius, 0, Math.PI * 2, true);
+        ctx.stroke(); 
+    }
+}
+
+//functions//
+function initcanvas() {
+
+    canvas = document.getElementById('tutorial');
+    ctx = canvas.getContext('2d');
+    ctx.canvas.width  = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    draw();
+}
+
+function draw() {
+
+    var list = new Array();
+   
+    for (var i = 0; i < 8; i++) {
+      for (var j = 0; j < 8; j++) {
+        
+        var curr = new Circle(i,j);
+        curr.draw();
+        list.push(curr);
+
       }
     }
 }
+
+/*canvas.addEventListener('click', function(e) {
+    
+})*/
