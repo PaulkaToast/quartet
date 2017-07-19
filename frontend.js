d791669e5c834666fb8264795998010e1fb506bb
Paula Toth <paulatoth18@aim.com>
2017-07-19 15:12:46 -0400

    Added dynamic resize of circle on size of window.

--- frontend.js
+++ frontend.js
@@ -3,15 +3,16 @@
 //Classes//
 class Circle {
 
-    constructor(x, y) {
-        this.radius = 30; //radius 
-        this.x = 40 + x * 70; //x cordinate
-        this.y = 40 + y * 80; //y cordinate
+    constructor(x, y, cSize) {
+        this.radius = 30*(cSize/570); //radius
+        this.x = (40 + x * 70)*(cSize/570); //x cordinate
+        this.y = (40 + y * 70)*(cSize/570); //y cordinate
         this.r = 0; //red
         this.b = Math.floor(255 - 25 * x); //blue
         this.g = Math.floor(255 - 5 * y); //green
         this.a = 0.5; //aplha
         this.af = 0.1; //aplha fill
+        this.cSize = cSize;
         this.clicked = false;
 
         this.shape = new createjs.Shape();
@@ -26,14 +27,14 @@ class Circle {
     draw() {
         this.shape.graphics.clear()
         .beginFill('rgba('
-            + this.r + ','              
-            + this.g + ',' 
+            + this.r + ','
+            + this.g + ','
             + this.b + ','
             + this.af + ')')
-        .setStrokeStyle(3, 'round', 'round')
+        .setStrokeStyle(3*(this.cSize/570), 'round', 'round')
         .beginStroke('rgba('
-            + this.r + ','              
-            + this.g + ',' 
+            + this.r + ','
+            + this.g + ','
             + this.b + ','
             + this.a + ')')
         .drawCircle(0, 0, this.radius);
@@ -67,7 +68,7 @@ function initcanvas() {
     ctx = canvas.getContext('2d');
     ctx.canvas.width  = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
-    
+
     draw();
 }
 
@@ -75,12 +76,18 @@ function draw() {
     stage = new createjs.Stage(canvas);
     stage.enableMouseOver(30);
     var list = new Array();
-   
+    var cSize = 0;
+    if( ctx.canvas.width > ctx.canvas.height){
+      cSize = ctx.canvas.height;
+    }else{
+      cSize = ctx.canvas.width;
+    }
+
     for (var i = 0; i < 8; i++) {
-      for (var j = 0; j < 8; j++) {  
-        var curr = new Circle(i, j);
+      for (var j = 0; j < 8; j++) {
+        var curr = new Circle(i, j, cSize);
         curr.draw();
-        list.push(curr); 
+        list.push(curr);
       }
     }
     stage.update();
