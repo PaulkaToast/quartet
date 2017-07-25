4408fffd16493940c5323b97409080a01f02bb18
Paula Toth <paulatoth18@aim.com>
2017-07-25 15:36:36 -0400

    Added frontend for play, stop, and pause buttons.

--- frontend.js
+++ frontend.js
@@ -1,12 +1,234 @@
 //variables//
 
 //Classes//
+class PauseButton {
+
+    constructor(cSize) {
+      this.x = 150*(cSize/650);
+      this.y = cSize - 70*(cSize/650);
+      this.length = 51*(cSize/650);
+      this.width = 17*(cSize/650);
+      this.r = 247;
+      this.b = 0;
+      this.g = 239;
+      this.a = 0.5; //aplha
+      this.af = 0.1; //aplha fill
+      this.cSize = cSize;
+      this.clicked = false;
+
+      this.shape = new createjs.Shape();
+      this.shape.x = 0;
+      this.shape.y = 0;
+      stage.addChild(this.shape);
+      this.shape.on("click", this.onClick.bind(this));
+      this.shape.on("mouseover", this.mouseOver.bind(this));
+      this.shape.on("mouseout", this.mouseOut.bind(this));
+    }
+    draw() {
+        this.shape.graphics.clear()
+        .beginFill('rgba('
+            + this.r + ','
+            + this.g + ','
+            + this.b + ','
+            + this.af + ')')
+        .setStrokeStyle(3*(this.cSize/650), 'round', 'round')
+        .beginStroke('rgba('
+            + this.r + ','
+            + this.g + ','
+            + this.b + ','
+            + this.a + ')')
+        .drawRect(this.x + this.width*2, this.y, this.width, this.length)
+        .drawRect(this.x + this.width*4, this.y, this.width, this.length)
+        .endFill()
+        .endStroke();
+
+
+    }
+    drawAndUpdate(){
+        this.draw();
+        stage.update();
+    }
+    unclick(){
+      if (this.clicked == true) {
+          this.af = 0.1;
+      }
+      this.clicked = false;
+      this.drawAndUpdate();
+    }
+    onClick(e) {
+        if (!this.clicked) {
+            this.af = 0.5;
+        } else {
+            this.af = 0.1;
+        }
+        play.unclick();
+        stop.unclick();
+        this.clicked = !this.clicked;
+        this.drawAndUpdate();
+    }
+    mouseOver(e) {
+        this.a = 1;
+        this.drawAndUpdate();
+    }
+    mouseOut(e) {
+        this.a = 0.5;
+        this.drawAndUpdate();
+    }
+}
+class StopButton {
+
+    constructor(cSize) {
+      this.x = 20*(cSize/650);
+      this.y = cSize - 70*(cSize/650);
+      this.side = 51*(cSize/650);
+      this.r = 247;
+      this.b = 0;
+      this.g = 0;
+      this.a = 0.5; //aplha
+      this.af = 0.1; //aplha fill
+      this.cSize = cSize;
+      this.clicked = false;
+
+      this.shape = new createjs.Shape();
+      this.shape.x = 0;
+      this.shape.y = 0;
+      stage.addChild(this.shape);
+      this.shape.on("click", this.onClick.bind(this));
+      this.shape.on("mouseover", this.mouseOver.bind(this));
+      this.shape.on("mouseout", this.mouseOut.bind(this));
+    }
+    draw() {
+        this.shape.graphics.clear()
+        .beginFill('rgba('
+            + this.r + ','
+            + this.g + ','
+            + this.b + ','
+            + this.af + ')')
+        .setStrokeStyle(3*(this.cSize/650), 'round', 'round')
+        .beginStroke('rgba('
+            + this.r + ','
+            + this.g + ','
+            + this.b + ','
+            + this.a + ')')
+        .drawRect(this.x, this.y, this.side, this.side)
+        .endFill()
+        .endStroke();
+
+
+    }
+    drawAndUpdate(){
+        this.draw();
+        stage.update();
+    }
+    unclick(){
+      if (this.clicked == true) {
+          this.af = 0.1;
+      }
+      this.clicked = false;
+      this.drawAndUpdate();
+    }
+    onClick(e) {
+        if (!this.clicked) {
+            this.af = 0.5;
+        } else {
+            this.af = 0.1;
+        }
+        play.unclick();
+        pause.unclick();
+        this.clicked = !this.clicked;
+        this.drawAndUpdate();
+    }
+    mouseOver(e) {
+        this.a = 1;
+        this.drawAndUpdate();
+    }
+    mouseOut(e) {
+        this.a = 0.5;
+        this.drawAndUpdate();
+    }
+}
+class PlayButton {
+
+    constructor(cSize) {
+      this.x = 100*(cSize/650);
+      this.y = cSize - 70*(cSize/650);
+      this.side = 51*(cSize/650);
+      this.r = 0;
+      this.b = 27;
+      this.g = 237;
+      this.a = 0.5; //aplha
+      this.af = 0.1; //aplha fill
+      this.cSize = cSize;
+      this.clicked = false;
+
+      this.shape = new createjs.Shape();
+      this.shape.x = 0;
+      this.shape.y = 0;
+      stage.addChild(this.shape);
+      this.shape.on("click", this.onClick.bind(this));
+      this.shape.on("mouseover", this.mouseOver.bind(this));
+      this.shape.on("mouseout", this.mouseOut.bind(this));
+    }
+    draw() {
+        this.shape.graphics.clear()
+        .beginFill('rgba('
+            + this.r + ','
+            + this.g + ','
+            + this.b + ','
+            + this.af + ')')
+        .setStrokeStyle(3*(this.cSize/650), 'round', 'round')
+        .beginStroke('rgba('
+            + this.r + ','
+            + this.g + ','
+            + this.b + ','
+            + this.a + ')')
+        .moveTo(this.x,this.y)
+        .lineTo(this.x,this.y + this.side)
+        .lineTo(this.x + this.side, this.y + (this.side/2))
+        .lineTo(this.x,this.y)
+        .endFill()
+        .endStroke();
+
+
+    }
+    drawAndUpdate(){
+        this.draw();
+        stage.update();
+    }
+    unclick(){
+      if (this.clicked == true) {
+          this.af = 0.1;
+      }
+      this.clicked = false;
+      this.drawAndUpdate();
+    }
+    onClick(e) {
+        if (!this.clicked) {
+            this.af = 0.5;
+        } else {
+            this.af = 0.1;
+        }
+        stop.unclick();
+        pause.unclick();
+        this.clicked = !this.clicked;
+        this.drawAndUpdate();
+    }
+    mouseOver(e) {
+        this.a = 1;
+        this.drawAndUpdate();
+    }
+    mouseOut(e) {
+        this.a = 0.5;
+        this.drawAndUpdate();
+    }
+}
+
 class Circle {
 
     constructor(i, j, cSize) {
-        this.radius = 30*(cSize/570); //radius
-        this.x = (40 + i * 70)*(cSize/570); //x cordinate
-        this.y = (40 + j * 70)*(cSize/570); //y cordinate
+        this.radius = 30*(cSize/650); //radius
+        this.x = (40 + i * 70)*(cSize/650); //x cordinate
+        this.y = (40 + j * 70)*(cSize/650); //y cordinate
         this.r = 0; //red
         this.b = Math.floor(255 - 25 * i); //blue
         this.g = Math.floor(255 - 5 * j); //green
@@ -31,13 +253,15 @@ class Circle {
             + this.g + ','
             + this.b + ','
             + this.af + ')')
-        .setStrokeStyle(3*(this.cSize/570), 'round', 'round')
+        .setStrokeStyle(3*(this.cSize/650), 'round', 'round')
         .beginStroke('rgba('
             + this.r + ','
             + this.g + ','
             + this.b + ','
             + this.a + ')')
-        .drawCircle(0, 0, this.radius);
+        .drawCircle(0, 0, this.radius)
+        .endFill()
+        .endStroke();
     }
     drawAndUpdate(){
         this.draw();
@@ -97,5 +321,12 @@ function draw() {
         list.push(curr);
       }
     }
+    play = new PlayButton(cSize);
+    play.draw();
+    stop = new StopButton(cSize);
+    stop.draw();
+    pause = new PauseButton(cSize);
+    pause.draw();
+
     stage.update();
 }
